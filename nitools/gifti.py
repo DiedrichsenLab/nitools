cd725f9ac087eff566d008eb91851df87b366030
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-09-25 16:03:53 -0400

    Added assert gifti

--- nitools/gifti.py
+++ nitools/gifti.py
@@ -85,6 +85,16 @@ def make_label_gifti(
         gifti (GiftiImage): Label gifti image
 
     """
+    if labels is not None:
+        # Check that any combination of labels, label_names, column_names and label_RGBA are the same length if given
+        if label_names is not None:
+            assert len(labels) == len(label_names), "labels and label_names must be the same length"
+        if column_names is not None:
+            assert len(labels) == len(column_names), "labels and column_names must be the same length"
+        if label_RGBA is not None:
+            assert len(labels) == len(label_RGBA), "labels and label_RGBA must be the same length"
+         
+
     num_verts, num_cols = data.shape
     if labels is None:
         labels = np.unique(data)
