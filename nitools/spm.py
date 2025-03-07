3c98de10a2d463d85b72dffb6b5ce942d0302355
Marco <97095997+mnlmrc@users.noreply.github.com>
2025-03-07 11:50:10 -0500

    added get_ciftis

--- nitools/spm.py
+++ nitools/spm.py
@@ -12,6 +12,8 @@ spm.get_info_from_spm_mat()
 from __future__ import annotations
 from os.path import normpath, dirname
 import numpy as np
+import nibabel as nb
+from nibabel import cifti2
 import nitools as nt
 from scipy.io import loadmat
 import pandas as pd
@@ -200,3 +202,79 @@ class SpmGlm:
         info = pd.DataFrame({'reg_name': self.beta_names[indx], 'run_number': self.run_number[indx]})
         
         return beta[indx,:], info, data_filt, data_hat, data_adj, residuals
+
+    def get_ciftis(self, mask=(None, None), TR=1000, extra=None):
+        """
+        Creates Cifti images to store the output of rerun_glm from an ROI defined in both hemispheres
+
+        Args:
+            mask ((str, str) tuple):
+                Path to the L and R (in this order) .nii ROI mask.
+                E.g. ('path/to/left_S1.nii', 'path/to/right_S1.nii')
+            TR (int):
+                Temporal resolution in milliseconds
+            extra (dict):
+                Extra information to store in Cifti images.
+                E.g., name of ROI: extra['name'] = 'S1'
+        Returns:
+            img_beta (CiftiImage): beta coefficients (PxQ), save as *.dscalar.nii
+            img_raw (CiftiImage): raw time series data (TxP), save as *.dtseries.nii
+            img_filt (CiftiImage): filtered time series data (TxP), save as *.dtseries.nii
+            img_hat (CiftiImage): predicted time series data (TxP), save as *.dtseries.nii
+                This is predicted only using regressors of interest (without the constant or other nuisance regressors)
+            img_adj (CiftiImage): adjusted time series data (TxP), save as *.dtseries.nii
+                This is filtered timeseries with constants and other nuisance regressors substrated out
+            img_res (CiftiImage): residuals (TxP), save as *.dtseries.nii
+        """
+
+        data = {name: [] for name in ["vox", "raw", "beta", "y_filt", "y_hat", "y_adj", "residuals"]}
+        struct = ['cortex_left', 'cortex_right']
+        for m, s in zip(mask, struct):
+            if m is not None:
+                # load mask
+                mask_img = nb.load(m)
+                coords = nt.get_mask_coords(mask_img)
+
+                # get raw data
+                raw_tmp = nt.sample_images(self.rawdata_files, coords)
+                data['raw'].append(raw_tmp)
+
+                # rerun glm
+                beta_tmp, info, y_filt_tmp, y_hat_tmp, y_adj_tmp, residuals_tmp = self.rerun_glm(raw_tmp)
+                data['beta'].append(beta_tmp)
+                data['y_filt'].append(y_filt_tmp)
+                data['y_hat'].append(y_hat_tmp)
+                data['y_adj'].append(y_adj_tmp)
+                data['residuals'].append(residuals_tmp)
+
+                data['vox'].append(cifti2.BrainModelAxis(
+                    name=s,
+                    voxel=nt.coords_to_voxelidxs(coords, mask_img).T,
+                    affine=mask_img.affine,
+                    volume_shape=mask_img.shape)
+                )
+
+        raw = np.hstack(data['raw'])
+        beta = np.hstack(data['beta'])
+        y_filt = np.hstack(data['y_filt'])
+        y_hat = np.hstack(data['y_hat'])
+        y_adj = np.hstack(data['y_adj'])
+        residuals = np.hstack(data['residuals'])
+
+        vox = data['vox'][0] + data['vox'][1]
+
+        series = cifti2.SeriesAxis(start=0, step=TR, size=raw.shape[0])
+
+        header = cifti2.Cifti2Header.from_axes((series, vox))
+        img_raw = cifti2.Cifti2Image(dataobj=raw, header=header, extra=extra)
+        img_filt = cifti2.Cifti2Image(dataobj=y_filt, header=header, extra=extra)
+        img_hat = cifti2.Cifti2Image(dataobj=y_hat, header=header, extra=extra)
+        img_adj = cifti2.Cifti2Image(dataobj=y_adj, header=header, extra=extra)
+        img_res = cifti2.Cifti2Image(dataobj=residuals, header=header, extra=extra)
+
+        scalar = cifti2.ScalarAxis(name=info.reg_name)
+        header = cifti2.Cifti2Header.from_axes((scalar, vox))
+        img_beta = cifti2.Cifti2Image(dataobj=beta, header=header, extra=extra)
+
+        return img_raw, img_beta, img_filt, img_hat, img_adj, img_res
+    
